d204d4d0f85ed03339c1e6fdc8d5c7439b7e1c91
vqr sn <vqrsns@gmail.com>
2023-09-20 22:27:14 -0600

    Lint rule does not work well with rhf

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -21,6 +21,7 @@ const config = {
 		"@typescript-eslint/no-unsafe-assignment": "off",
 		"@typescript-eslint/no-unsafe-argument": "off",
 		"@typescript-eslint/no-explicit-any": "off",
+		"@typescript-eslint/no-misused-promises": "off",
 		"@typescript-eslint/consistent-type-imports": [
 			"warn",
 			{
