90236e0e74a650b7bc21eba88eea9523b69f4d73
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-17 09:26:44 -0600

    New grid variant: blue-border

--- src/theme/theme.ts
+++ src/theme/theme.ts
@@ -1,12 +1,22 @@
-import { Divider, createTheme } from "@mantine/core"
+import { Card, Divider, createTheme } from "@mantine/core"
+
+import cardClasses from "./Card.module.css"
+
+declare module "@mantine/core" {
+	export interface CardProps {
+		variant?: "blue-border"
+	}
+}
 
 export const theme = createTheme({
 	components: {
 		Divider: Divider.extend({
 			defaultProps: {
-				color: "blue.8",
 				variant: "solid",
 			},
+		}),
+		Card: Card.extend({
+			classNames: cardClasses,
 		}),
 	},
 })
