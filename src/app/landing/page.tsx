c8e4a9527adf55fd51d5b433c7f448c39d18484a
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-17 21:39:05 -0600

    Set up scaffolding for app router

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import { type Metadata } from "next"
+
+export const metadata: Metadata = {
+	title: "Landing",
+	description: "Ease your life with the 5 Stars Discord bot for automating tasks.",
+	keywords: ["discord", "bot", "automation", "reminders", "translation"],
+}
+
+export default function Page() {
+	return <></>
+}
