50e1185beb1440c23ab7eb1e24e4586d090e31ff
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-22 14:55:23 -0600

    Made abstract interface for cleaned data

---
+++ src/types/types.ts
@@ -0,0 +1,43 @@
+export interface SupportedLanguageData {
+	id: string | number
+	name: string
+	icon: string
+	discordIconLabel: string
+	countryName: string
+	supported: boolean
+}
+
+interface ISourceData {
+	primary_language: string
+	full_data: object
+}
+
+abstract class CiaData implements ISourceData {
+	abstract primary_language: string
+	abstract full_data: CiaFullData
+}
+
+interface CiaFullData {
+	country: string
+	languages: Record<string, string>
+}
+
+abstract class WikipediaData implements ISourceData {
+	abstract primary_language: string
+	abstract full_data: WikipediaFullData
+}
+
+interface WikipediaFullData {
+	widely_spoken: string
+	country_region: string
+	minority_language: string
+	national_language: string
+	official_language: string
+	regional_language: string
+}
+
+export interface CountryResult {
+	country_name: string
+	cia_data: CiaData
+	wikipedia_data: WikipediaData
+}
