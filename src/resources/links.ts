f38c1ab8c17159f3bb9f39091186feb766fbca8d
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-22 14:55:00 -0600

    Reorganized link location

--- src/resources/links.ts
+++ src/resources/links.ts
@@ -1,1 +1,8 @@
 export const DeepLLink = `https://www.deepl.com/en/docs-api`
+
+export const defaultLinks = [
+	{ label: "Home", link: "/" },
+	{ label: "Github", link: "https://github.com/rsns-0/5-star-backend" },
+	{ label: "Developers", link: "/developers" },
+	{ label: "About", link: "/about" },
+]
