d93bc4a12e58fad755efc38bd6c1b9a1faa55e1d
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-24 15:57:36 -0600

    Updated links

--- src/resources/links.ts
+++ src/resources/links.ts
@@ -1,9 +1,15 @@
 export const DeepLLink = `https://www.deepl.com/en/docs-api`
 
+type LinkData = {
+	label: string
+	link: string
+} 
+
 export const defaultLinks = [
 	{ label: "Home", link: "/" },
 	{ label: "Github", link: "https://github.com/rsns-0/5-star-backend" },
 	{ label: "Developers", link: "/developers" },
 	{ label: "Supported Languages", link: "/languages" },
+	{ label: "Reminder Table", link: "/reminder-table" },
 	{ label: "About", link: "/about" },
-]
+] satisfies LinkData[]
