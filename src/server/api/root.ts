03724d7bda20ed3044f333d16d9c26aa799f0447
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-25 01:53:24 -0600

    Create helper for using TRPC with server side helpers

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -4,6 +4,9 @@ import { developerInfoRouter } from "./routers/developerInfo";
 import { languagesRouter } from "./routers/languages"
 import { discordRouter } from "./routers/discordRouter"
 import { reminderRouter } from "./routers/reminderRouter"
+import { getServerAuthSession } from "../auth"
+import { type GetServerSidePropsContext } from "next"
+import { db } from "../db"
 
 /**
  * This is the primary router for your server.
@@ -17,6 +20,15 @@ export const appRouter = createTRPCRouter({
 	discordRouter: discordRouter,
 	reminderRouter: reminderRouter,
 })
+
+export const createAppRouterFromRequestContext = (ctx: GetServerSidePropsContext) => {
+	return getServerAuthSession(ctx).then((session) => {
+		return appRouter.createCaller({
+			session: session,
+			db: db,
+		})
+	})
+}
 
 // export type definition of API
 export type AppRouter = typeof appRouter
