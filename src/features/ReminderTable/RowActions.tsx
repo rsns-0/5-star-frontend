cde2ae9d7e221ab2e0a486ec0fe9674754a5263a
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-25 14:13:20 -0600

    Minor table refactors

--- src/features/ReminderTable/RowActions.tsx
+++ src/features/ReminderTable/RowActions.tsx
@@ -4,36 +4,35 @@ import { EditButton } from "../../components/buttons/EditButton"
 import { DeleteButton } from "../../components/buttons/DeleteButton"
 import { useDeleteItem, useOpenReminderTableEditModal } from "../../hooks/reminderTable"
 import { observer } from "mobx-react"
-import { notifications } from "@mantine/notifications"
 
 const RowActionsRenderer = ({ data }: ICellRendererParams<ReminderData, string>) => {
 	if (!data) {
-		return <></>
+		return
 	}
 
 	return <RowActionsImpl data={data} />
 }
 
 const RowActionsImpl = ({ data }: { data: ReminderData }) => {
-	const openEditModal = useOpenReminderTableEditModal()
-	const deleteReminder = useDeleteItem(data.id)
-	const handleDelete = () => {
-		deleteReminder()
-		notifications.show({
-			message: `Reminder ${data.id} deleted`,
-		})
-	}
+	const { handleDelete, openEditModal } = useRowActions(data)
 
 	return (
 		<>
-			<EditButton
-				onClick={() => {
-					openEditModal(data)
-				}}
-			/>{" "}
+			<EditButton onClick={openEditModal} />
 			<DeleteButton onClick={handleDelete} />
 		</>
 	)
+}
+
+const useRowActions = (data: ReminderData) => {
+	const openEditModal = useOpenReminderTableEditModal(data)
+
+	const handleDelete = useDeleteItem(data.id)
+
+	return {
+		openEditModal,
+		handleDelete,
+	}
 }
 
 export const RowActions = observer(RowActionsRenderer)
