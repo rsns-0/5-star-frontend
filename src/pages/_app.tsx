9072bebef5cf4db7c5e3af167776e7759bfe35f2
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-25 01:52:04 -0600

    Update globals needed for Mantine

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,19 @@ import { api } from "~/utils/api"
 
 import "~/styles/globals.css"
 import "@mantine/core/styles.css"
+import "@mantine/dates/styles.css"
+import "@mantine/notifications/styles.css"
 import AppProvider from "../providers/AppProvider"
 import Head from "next/head"
 import { Navbar } from "../components/Navbar/Navbar"
 import { defaultLinks } from "../resources/links"
 import { Header } from "../components/Header/Header"
+
+import dayjs from "dayjs"
+import customParseFormat from "dayjs/plugin/customParseFormat"
+import { Notifications } from "@mantine/notifications"
+
+dayjs.extend(customParseFormat)
 
 const MyApp: AppType<{ session: Session | null }> = ({
 	Component,
@@ -29,8 +37,10 @@ const MyApp: AppType<{ session: Session | null }> = ({
 
 			<SessionProvider session={session}>
 				<AppProvider>
+					<Notifications />
 					<Navbar links={defaultLinks} />
 					<Header links={defaultLinks} />
+
 					<Component {...pageProps} />
 				</AppProvider>
 			</SessionProvider>
