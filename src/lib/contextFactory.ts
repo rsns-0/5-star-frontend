0b79d0cb6964e8c57b7ffc8b3ee4a27090c0b0ca
sn <126475703+rsns-0@users.noreply.github.com>
2023-10-25 01:53:51 -0600

    Util to create contexts

---
+++ src/lib/contextFactory.ts
@@ -0,0 +1,20 @@
+import { createContext, useContext } from "react"
+
+export const contextFactory = <T>(
+	initialValue?: T,
+	fallbackDisplayName = "NO FALLBACK DISPLAY NAME"
+) => {
+	const ctx = createContext<T | null>(initialValue ?? null)
+	const useHook = () => {
+		const ctxValue = useContext(ctx)
+		if (!ctxValue) {
+			throw new Error(
+				`use${ctx.displayName ?? fallbackDisplayName}Context must be used within a ${
+					ctx.displayName ?? fallbackDisplayName
+				}Provider`
+			)
+		}
+		return ctxValue
+	}
+	return [ctx.Provider, useHook] as const
+}
