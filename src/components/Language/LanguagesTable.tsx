ead9a65e25d4cc38669fc4a878c6148a3dac5487
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-18 14:07:35 -0700

    add language/country/emoji mappings table

--- src/components/Language/LanguagesTable.tsx
+++ src/components/Language/LanguagesTable.tsx
@@ -5,11 +5,12 @@ import "ag-grid-community/styles/ag-theme-alpine.css" // Optional theme CSS
 import { type ICellRendererParams, type ColDef } from "ag-grid-community"
 import { type SupportedLanguageData } from "../../types/types"
 import { useRef } from "react"
-import { Center, Stack, TextInput } from "@mantine/core"
+import { Button, Center, Group, Stack, TextInput, Tooltip } from "@mantine/core"
 import { useInputState } from "@mantine/hooks"
 import { TwemojiImage } from "../Tweemoji/TweemojiImage"
 
 import styles from "./LanguagesTable.module.css"
+import { ExportDataButton, jsonDataProps } from "../buttons/ExportDataButton"
 
 type LanguagesTableProps = {
 	data: SupportedLanguageData[]
@@ -23,33 +24,69 @@ const columnDefs: ColDef<SupportedLanguageData>[] = [
 		},
 		filter: false,
 		sortable: false,
+		headerTooltip: "Icon of the country's flag.",
 		width: 100,
 	},
 	{
-		field: "countryName",
+		field: "id",
+		headerTooltip: "The ID of the country.",
+		sortable: false,
 	},
 	{
-		field: "name",
-		headerName: "Language",
+		field: "countryName",
+		headerTooltip: "The name of the country.",
+	},
+	{
+		field: "language",
+		headerTooltip: "The name of the language.",
 	},
 	{
 		field: "discordIconLabel",
+		headerTooltip: "The Discord emoji label for the language.",
+	},
+	{
+		field: "ISO1",
+		headerTooltip: "The ISO 639-1 code for the language.",
+	},
+	{
+		field: "ISO2",
+		headerTooltip: "The ISO 639-2 code for the language.",
+	},
+	{
+		field: "ISO2B",
+		headerTooltip: "The ISO 639-2/B code for the language.",
+	},
+	{
+		field: "CCA2",
+		headerTooltip: "The ISO 3166-1 alpha-2 code for the country.",
+	},
+	{
+		field: "CCA3",
+		headerTooltip: "The ISO 3166-1 alpha-3 code for the country.",
+	},
+
+	{
+		field: "weight",
+		headerTooltip:
+			"Reflects the confidence that the language is the primary language of the country. Records with higher scores are more likely to be accurate and less volatile to change.",
 	},
 	{
 		field: "supported",
+		headerTooltip: "Whether the language can be translated.",
 	},
 ]
 
 const defaultColDef: ColDef<SupportedLanguageData> = {
 	flex: 1,
 	minWidth: 100,
-	resizable: false,
+	resizable: true,
 	sortable: true,
 	filter: true,
-	lockVisible: true,
+	lockVisible: false,
+	tooltipValueGetter: (s) => s.value,
 }
 
-const useInput = (initialValue = "") => {
+const useQuickFilterInput = (initialValue = "") => {
 	const [value, setValue] = useInputState(initialValue)
 
 	const inputProps = {
@@ -66,21 +103,44 @@ const useInput = (initialValue = "") => {
 
 export const LanguagesTable = ({ data }: LanguagesTableProps) => {
 	const gridRef = useRef<AgGridReact<SupportedLanguageData>>(
-		null as any as AgGridReact<SupportedLanguageData>
+		null as unknown as AgGridReact<SupportedLanguageData>
 	)
 
-	const { value, inputProps } = useInput()
+	const { value, inputProps, setValue } = useQuickFilterInput()
+
+	const onRevealClick = () => {
+		gridRef.current.columnApi.setColumnsVisible(
+			columnDefs.map((s) => s.field!),
+			true
+		)
+	}
+
+	const onResetFilterClick = () => {
+		gridRef.current.api.setFilterModel(null)
+		setValue("")
+	}
 
 	return (
 		<Center>
 			<Stack gap="xl">
-				<TextInput
-					styles={{ label: { color: "white" } }}
-					label="Quick Filter"
-					placeholder="Search the entire table..."
-					w={400}
-					{...inputProps}
-				/>
+				<Group align="end">
+					<TextInput
+						styles={{ label: { color: "white" } }}
+						label="Quick Filter"
+						placeholder="Search the entire table..."
+						w={400}
+						{...inputProps}
+					/>
+					<ExportDataButton data={data} />
+					<ExportDataButton data={data} {...jsonDataProps} />
+					<Tooltip label="Reveals all columns that were hidden through drag and drop.">
+						<Button onClick={onRevealClick}>Reveal Columns</Button>
+					</Tooltip>
+					<Tooltip label="Resets all filters.">
+						<Button onClick={onResetFilterClick}>Reset filters</Button>
+					</Tooltip>
+				</Group>
+
 				<div className={`ag-theme-alpine-dark ${styles.tableContainer}`}>
 					<AgGridReact<SupportedLanguageData>
 						ref={gridRef}
@@ -96,6 +156,8 @@ export const LanguagesTable = ({ data }: LanguagesTableProps) => {
 						ensureDomOrder={true}
 						quickFilterText={value}
 						rowData={data}
+						tooltipShowDelay={400}
+						enableCellTextSelection={true}
 						columnDefs={columnDefs}
 						defaultColDef={defaultColDef}
 					/>
--- src/server/api/routers/languages.ts
+++ src/server/api/routers/languages.ts
@@ -1,45 +1,29 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
-// import { mockLanguageData } from "../../../resources/mockLanguageData"
 import { type SupportedLanguageData } from "../../../types/types"
-import { db2 } from "../../db"
-import { sql } from "kysely"
+import { db2 } from "../../db2"
+import { getCountryPrimaryLanguageData } from "../../queries/getCountryPrimaryLanguageData"
 
 export const languagesRouter = createTRPCRouter({
 	getSupportedLanguages: publicProcedure.query(async (): Promise<SupportedLanguageData[]> => {
 		return db2
-			.selectFrom("flag_language")
+			.selectFrom("flag_language_materialized")
 			.select([
 				"id",
 				"country_name as countryName",
-				sql<string>`'🇺🇸'`.as("icon"),
+				"flag_emoji as icon",
 				"flag_key as discordIconLabel",
-				"primary_language as name",
+				"primary_language as language",
 				"is_supported_by_deep_l as supported",
+				"cca2 as CCA2",
+				"cca3 as CCA3",
+				"iso1 as ISO1",
+				"iso2 as ISO2",
+				"iso2b as ISO2B",
+				"primary_language_weight as weight",
 			])
-			.limit(20000)
+			.orderBy("countryName")
+			.limit(10000)
 			.execute()
-
-		
 	}),
+	getCountryPrimaryLanguageData: publicProcedure.query(getCountryPrimaryLanguageData),
 })
-// const result = await ctx.db.flag_language.findMany({
-		// 	select: {
-		// 		id: true,
-		// 		country_name: true,
-		// 		cca2: true,
-		// 		cca3: true,
-		// 		is_supported_by_deep_l: true,
-		// 		primary_language: true,
-		// 		iso1: true,
-		// 		iso2: true,
-		// 		iso2b: true,
-		// 		flag_key: true,
-		// 	},
-		// })
-
-		// const res: SupportedLanguageData[] = await new Promise((resolve) => {
-		// 	setTimeout(() => {
-		// 		resolve(mockLanguageData)
-		// 	}, 16)
-		// })
-		// return res
