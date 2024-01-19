ffe3be173b40122d92e7a671717126515bab3691
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-18 17:24:18 -0700

    inline overly abstracted components

--- src/pages/testing/test-landing-page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-import LandingPageLayout from "../../features/LandingPage/LandingPage"
-
-const testLandingPage = () => {
-	return <LandingPageLayout />
-}
-
-export default testLandingPage
