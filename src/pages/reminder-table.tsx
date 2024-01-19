ba038a7c83f93e72d833382141e37f6bef1940fc
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-18 17:25:45 -0700

    Add title to reminder table page

--- src/pages/reminder-table.tsx
+++ src/pages/reminder-table.tsx
@@ -1,14 +1,8 @@
 import { Center, Loader } from "@mantine/core"
 import { ReminderTable } from "../features/ReminderTable/ReminderTable"
 import { api } from "../utils/api"
+import { TitleText } from "../components/typography/TitleText"
 
-/**
- * avoid waterfall but also maintain separate cache for each query since reminders will get invalidated often but not the channels https://tkdodo.eu/blog/seeding-the-query-cache#:~:text=A%20waterfall%20describes%20a%20situation,complete%20before%20firing%20another%20request.&text=In%20both%20variants%2C%20React%20Query,off%20data%20fetching%20in%20parallel.
- *
- * cannot use trpc with SSR, breaks NextAuth & other bugs https://github.com/trpc/trpc/issues/596
- *
- *
- */
 export default function Page() {
 	const { isLoading, isError, data } = api.reminderRouter.get.getAllReminders.useQuery()
 	void api.useContext().discordRouter.getGuildsAndTextBasedChannelsOfUser.prefetch()
@@ -24,6 +18,13 @@ export default function Page() {
 			</Center>
 		)
 
-	return <ReminderTable data={data} />
+	return (
+		<>
+			<Center mb={"xl"}>
+				<TitleText fz="2.5rem">Reminders</TitleText>
+			</Center>
+			<ReminderTable data={data} />
+		</>
+	)
 }
 
