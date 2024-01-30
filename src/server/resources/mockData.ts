92ece51678f0d1ef9a0b892fb098f074463d70a7
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-29 17:24:26 -0700

    update mockdata

--- src/server/resources/mockData.ts
+++ src/server/resources/mockData.ts
@@ -2,69 +2,49 @@ import { type ChannelData, type ReminderData } from "../../types/types"
 
 export const mockReminderData: ReminderData[] = [
 	{
-		time: new Date(),
 		channel_id: "channel1",
-		reminder_message: "Reminder 1",
-		discord_channels: {
-			name: "Channel 1",
-			discord_guilds: {
-				name: "Guild 1",
-			},
-			id: "1",
-		},
+		channel_name: "Channel 1",
+		guild_id: "guild1",
+		guild_name: "Guild 1",
 		id: 1,
+		reminder_message: "Reminder 1",
+		time: new Date("2024-01-28T09:00:00"),
 	},
 	{
-		time: new Date(),
 		channel_id: "channel2",
-		reminder_message: "Reminder 2",
-		discord_channels: {
-			name: "Channel 2",
-			discord_guilds: {
-				name: "Guild 2",
-			},
-			id: "2",
-		},
+		channel_name: "Channel 2",
+		guild_id: "guild2",
+		guild_name: "Guild 2",
 		id: 2,
+		reminder_message: "Reminder 2",
+		time: new Date("2024-01-28T10:00:00"),
 	},
 	{
-		time: new Date(),
 		channel_id: "channel3",
-		reminder_message: "Reminder 3",
-		discord_channels: {
-			name: "Channel 3",
-			discord_guilds: {
-				name: "Guild 3",
-			},
-			id: "3",
-		},
+		channel_name: "Channel 3",
+		guild_id: "guild3",
+		guild_name: "Guild 3",
 		id: 3,
+		reminder_message: "Reminder 3",
+		time: new Date("2024-01-28T11:00:00"),
 	},
 	{
-		time: new Date(),
 		channel_id: "channel4",
-		reminder_message: "Reminder 4",
-		discord_channels: {
-			name: "Channel 4",
-			discord_guilds: {
-				name: "Guild 4",
-			},
-			id: "4",
-		},
+		channel_name: "Channel 4",
+		guild_id: "guild4",
+		guild_name: "Guild 4",
 		id: 4,
+		reminder_message: "Reminder 4",
+		time: new Date("2024-01-28T12:00:00"),
 	},
 	{
-		time: new Date(),
 		channel_id: "channel5",
-		reminder_message: "Reminder 5",
-		discord_channels: {
-			name: "Channel 5",
-			discord_guilds: {
-				name: "Guild 5",
-			},
-			id: "5",
-		},
+		channel_name: "Channel 5",
+		guild_id: "guild5",
+		guild_name: "Guild 5",
 		id: 5,
+		reminder_message: "Reminder 5",
+		time: new Date("2024-01-28T13:00:00"),
 	},
 ]
 
@@ -72,76 +52,56 @@ export const mockChannelData: ChannelData = [
 	{
 		discord_channels: [
 			{
-				webhooks: [
-					{
-						id: "webhook1",
-					},
-				],
 				id: "channel1",
 				name: "Channel 1",
 			},
 		],
 		id: "1",
 		name: "Name 1",
+		icon_url: "https://cdn.discordapp.com/icons/1/1.png",
 	},
 	{
 		discord_channels: [
 			{
-				webhooks: [
-					{
-						id: "webhook2",
-					},
-				],
 				id: "channel2",
 				name: "Channel 2",
 			},
 		],
 		id: "2",
 		name: "Name 2",
+		icon_url: "https://cdn.discordapp.com/icons/1/1.png",
 	},
 	{
 		discord_channels: [
 			{
-				webhooks: [
-					{
-						id: "webhook3",
-					},
-				],
 				id: "channel3",
 				name: "Channel 3",
 			},
 		],
 		id: "3",
 		name: "Name 3",
+		icon_url: "https://cdn.discordapp.com/icons/1/1.png",
 	},
 	{
 		discord_channels: [
 			{
-				webhooks: [
-					{
-						id: "webhook4",
-					},
-				],
 				id: "channel4",
 				name: "Channel 4",
 			},
 		],
 		id: "4",
 		name: "Name 4",
+		icon_url: "https://cdn.discordapp.com/icons/1/1.png",
 	},
 	{
 		discord_channels: [
 			{
-				webhooks: [
-					{
-						id: "webhook5",
-					},
-				],
 				id: "channel5",
 				name: "Channel 5",
 			},
 		],
 		id: "5",
 		name: "Name 5",
+		icon_url: "https://cdn.discordapp.com/icons/1/1.png",
 	},
 ]
