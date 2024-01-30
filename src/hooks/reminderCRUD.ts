93a15d9eb284e11de7ba77bee57f20b0598fd335
sn <126475703+rsns-0@users.noreply.github.com>
2024-01-29 17:34:18 -0700

    implement show error message on client end

--- src/hooks/reminderCRUD.ts
+++ src/hooks/reminderCRUD.ts
@@ -1,3 +1,4 @@
+import { notifications } from "@mantine/notifications"
 import { api } from "../utils/api"
 
 export const useCreateReminder = () => {
@@ -17,7 +18,10 @@ export const useDeleteReminder = () => {
 				return prev?.filter((reminder) => reminder.id !== id) ?? []
 			})
 		},
-		onSettled() {
+		onSettled(item) {
+			if (item instanceof Error) {
+				showErrorMessage(item.message)
+			}
 			void utils.reminderRouter.get.invalidate()
 		},
 	})
@@ -29,18 +33,30 @@ export const useUpdateReminder = () => {
 	return api.reminderRouter.patch.updateReminder.useMutation({
 		onMutate(item) {
 			utils.reminderRouter.get.getAllReminders.setData(undefined, (prev) => {
-				return (
-					prev?.map((reminder) => {
-						if (reminder.id === item.id) {
-							return { ...reminder, ...item }
-						}
-						return reminder
-					}) ?? []
-				)
+				if (!prev) {
+					return []
+				}
+				return prev.map((reminder) => {
+					if (reminder.id === item.id) {
+						return { ...reminder, ...item }
+					}
+					return reminder
+				})
 			})
 		},
-		onSettled() {
+		onSettled(item) {
+			if (item instanceof Error) {
+				showErrorMessage(item.message)
+			}
 			void utils.reminderRouter.get.invalidate()
 		},
+	})
+}
+
+function showErrorMessage(message: string) {
+	notifications.show({
+		title: "Error",
+		message,
+		color: "red",
 	})
 }
