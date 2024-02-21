749b1629a7d145a47e45dbc732adac60410b985c
sn <126475703+rsns-0@users.noreply.github.com>
2024-02-21 16:26:42 -0700

    add bordered card to language header

--- src/features/languages/LanguagePageHeading.tsx
+++ src/features/languages/LanguagePageHeading.tsx
@@ -1,23 +1,26 @@
-import { Stack, Box, Text, Container, Center } from "@mantine/core"
+import { Stack, Box, Text, Center, Container } from "@mantine/core"
 import { TitleText } from "../../components/typography/TitleText"
+import { BorderCard } from "../../components/card/BorderCard"
 
 export function LanguagePageHeading() {
 	return (
 		<Center>
-			<Stack>
-				<Container>
-					<TitleText order={1}>Supported Languages</TitleText>
-				</Container>
-				<Box component="section" title="Description" maw="40rem">
-					<Text>
-						The following table provides data on the mappings between the country, its
-						associated flag emoji, and its estimated primary language. Only countries
-						and languages with mainstream ISO codes and countries with known spoken
-						languages will be displayed. Hover over the column headers for more
-						information.
-					</Text>
-				</Box>
-			</Stack>
+			<BorderCard>
+				<Stack>
+					<Container>
+						<TitleText order={1}>Supported Languages</TitleText>
+					</Container>
+					<Box component="section" title="Description" maw="40rem">
+						<Text>
+							The following table provides data on the mappings between the country,
+							its associated flag emoji, and its estimated primary language. Only
+							countries and languages with mainstream ISO codes and countries with
+							known spoken languages will be displayed. Hover over the column headers
+							for more information.
+						</Text>
+					</Box>
+				</Stack>
+			</BorderCard>
 		</Center>
 	)
 }
