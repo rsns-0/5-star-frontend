7d616f77ba5cc643cd0240a2e7418883de30edd9
sn <126475703+rsns-0@users.noreply.github.com>
2024-02-26 23:52:46 -0700

    adjust env var

--- src/env.mjs
+++ src/env.mjs
@@ -26,6 +26,7 @@ export const env = createEnv({
 			// VERCEL_URL doesn't include `https` so it cant be validated as a URL
 			process.env.VERCEL ? minStringSchema : z.string().url()
 		),
+
 		// Add `.min(1) on ID and SECRET if you want to make sure they're not empty
 		DISCORD_CLIENT_ID: z.string(),
 		DISCORD_CLIENT_SECRET: z.string(),
@@ -40,6 +41,7 @@ export const env = createEnv({
 	 */
 	client: {
 		// NEXT_PUBLIC_CLIENTVAR: z.string().min(1),
+		NEXT_PUBLIC_APP_URL: z.string().url(),
 	},
 
 	/**
@@ -50,6 +52,7 @@ export const env = createEnv({
 		TEST_USER: process.env.TEST_USER,
 		DATABASE_URL: process.env.DATABASE_URL,
 		NODE_ENV: process.env.NODE_ENV,
+		NEXT_PUBLIC_APP_URL: process.env.APP_URL,
 		NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
 		NEXTAUTH_URL: process.env.NEXTAUTH_URL,
 		DISCORD_CLIENT_ID: process.env.DISCORD_CLIENT_ID,
