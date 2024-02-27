96c7484e48bb5dea688ba80e12fcece4c405209b
sn <126475703+rsns-0@users.noreply.github.com>
2024-02-26 22:44:25 -0700

    fix language histogram not returning correct data, use materialized view
    also add minor schema adjustments

--- src/models/histogram.ts
+++ src/models/histogram.ts
@@ -1,12 +1,13 @@
 import { z } from "zod"
 
-export const histogramInputSchema = z
+export const paginationInputSchema = z
 	.object({
 		cursor: z.number().describe("ID-based cursor for pagination.").gte(0).optional(),
 		take: z.number().describe("The number of records to take").gte(0).lte(10000).optional(),
 	})
-	.optional()
 	.describe("Pagination input")
+
+export const histogramInputSchema = paginationInputSchema.optional()
 
 export const histogramEntrySchema = z
 	.object({
@@ -16,42 +17,46 @@ export const histogramEntrySchema = z
 			.describe("The number of times the label occurred within the dataset."),
 	})
 	.describe("Histogram entry")
+	.strict()
 
-export const histogramResultSchema = z
+const notNanSchema = z.coerce
+	.number()
+	.refine((s) => !isNaN(s), { message: "Value must not be NaN" })
+
+export const histogramDataSchema = z
 	.object({
-		id: z.coerce.number().int().describe("The entity ID."),
-		entries: histogramEntrySchema.array(),
-		sample_size: z.coerce
+		id: z.coerce
 			.number()
-			.describe("The total number of string representations for that entity."),
-		cluster_max: z.coerce
-			.number()
-			.describe("The frequency of the most frequent string representation."),
-		cluster_min: z.coerce
-			.number()
-			.describe("The frequency of the least frequent string representation."),
-		cluster_avg: z.coerce.number().describe("The average frequency value among clusters."),
-		cluster_stddev: z.coerce.number().describe("The standard deviation among clusters."),
-		cluster_count: z.coerce
-			.number()
-			.describe("The total number of string representation clusters."),
-		mode_count: z.coerce
-			.number()
-			.describe(
-				"The total number of string representation clusters that have the same frequency as the most frequent string representation."
-			),
-		mode_score: z.coerce
-			.number()
-			.describe(
-				"Score describing the confidence score of the top cluster relative to other significant clusters"
-			),
-		similarity_max: z.coerce.number().describe("The maximum similarity score."),
-		similarity_min: z.coerce.number().describe("The minimum similarity score."),
-		similarity_avg: z.coerce.number().describe("The average similarity score."),
-		similarity_stddev: z.coerce
-			.number()
-			.describe("The standard deviation among similarity scores."),
-		similarity_range: z.coerce.number().describe("The range of similarity scores."),
+			.int()
+			.refine((s) => !isNaN(s))
+			.describe("The entity ID."),
+		entries: histogramEntrySchema.array().nonempty(),
+		sample_size: notNanSchema.describe(
+			"The total number of string representations for that entity."
+		),
+		cluster_max: notNanSchema.describe(
+			"The frequency of the most frequent string representation."
+		),
+		cluster_min: notNanSchema.describe(
+			"The frequency of the least frequent string representation."
+		),
+		cluster_avg: notNanSchema.describe("The average frequency value among clusters."),
+		cluster_stddev: notNanSchema.describe("The standard deviation among clusters."),
+		cluster_range: notNanSchema.describe("The range of frequency values among clusters."),
+		cluster_count: notNanSchema.describe("The total number of string representation clusters."),
+		mode_count: notNanSchema.describe(
+			"The total number of string representation clusters that have the same frequency as the most frequent string representation."
+		),
+		mode_score: notNanSchema.describe(
+			"Score describing the confidence score of the top cluster relative to other significant clusters"
+		),
+		similarity_max: notNanSchema.describe("The maximum similarity score."),
+		similarity_min: notNanSchema.describe("The minimum similarity score."),
+		similarity_avg: notNanSchema.describe("The average similarity score."),
+		similarity_stddev: notNanSchema.describe("The standard deviation among similarity scores."),
+		similarity_range: notNanSchema.describe("The range of similarity scores."),
 	})
 	.describe("Histogram data")
-	.array()
+	.strict()
+
+export const histogramResultSchema = histogramDataSchema.array()
--- src/server/api/routers/histogramRouter.ts
+++ src/server/api/routers/histogramRouter.ts
@@ -1,5 +1,19 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc"
 import { histogramInputSchema, histogramResultSchema } from "../../../models/histogram"
+import { type z } from "zod"
+import { type Prisma } from "@prisma/client"
+
+function createHistogramQueryData(data: z.infer<typeof histogramInputSchema>) {
+	const cursor = data?.cursor ? { id: data.cursor } : undefined
+	const input = {
+		...data,
+		cursor,
+		orderBy: { id: "asc" },
+		take: data?.take ?? 500,
+		skip: cursor === undefined ? 0 : 1,
+	} as const satisfies Prisma.country_histogramFindManyArgs
+	return input
+}
 
 export const histogramRouter = createTRPCRouter({
 	getCountryHistogram: publicProcedure
@@ -14,13 +28,9 @@ export const histogramRouter = createTRPCRouter({
 		.input(histogramInputSchema)
 		.output(histogramResultSchema)
 		.query(async ({ ctx: { db }, input }) => {
-			const cursor = input?.cursor ? { id: input.cursor } : undefined
-			return (await db.country_histogram.findMany({
-				...input,
-				cursor,
-				orderBy: { id: "asc" },
-				take: input?.take ?? 500,
-			})) as any
+			return (await db.country_histogram_materialized.findMany(
+				createHistogramQueryData(input)
+			)) as any
 		}),
 	getLanguageHistogram: publicProcedure
 		.meta({
@@ -34,12 +44,8 @@ export const histogramRouter = createTRPCRouter({
 		.input(histogramInputSchema)
 		.output(histogramResultSchema)
 		.query(async ({ ctx: { db }, input }) => {
-			const cursor = input?.cursor ? { id: input.cursor } : undefined
-			return (await db.country_histogram.findMany({
-				...input,
-				cursor,
-				orderBy: { id: "asc" },
-				take: input?.take ?? 500,
-			})) as any
+			return (await db.language_histogram_materialized.findMany(
+				createHistogramQueryData(input)
+			)) as any
 		}),
 })
