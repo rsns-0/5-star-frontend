5048a9bfde83061768319f804ba0491d4dbfb731
sn <126475703+rsns-0@users.noreply.github.com>
2024-04-10 18:35:05 -0600

    Change discord button impl to link
    - Improved accessibility and capability to open in new tab

--- src/components/InviteDiscordButton/InviteDiscordButton.tsx
+++ src/components/InviteDiscordButton/InviteDiscordButton.tsx
@@ -2,8 +2,9 @@ import { FaDiscord } from "react-icons/fa"
 import classes from "./InviteDiscordButton.module.css"
 import { type ButtonProps } from "@mantine/core"
 import { type ReactNode } from "react"
-import { useRouter } from "next/router"
+
 import { DISCORD_INVITE_LINK } from "../../resources/links"
+import Link from "next/link"
 
 export interface InviteDiscordButtonProps extends ButtonProps {
 	text?: string
@@ -14,15 +15,14 @@ export default function InviteDiscordButton({
 	text = "Invite to Discord",
 	icon = <FaDiscord size={25} />,
 }: InviteDiscordButtonProps) {
-	const router = useRouter()
-	const onClick = async () => await router.push(DISCORD_INVITE_LINK)
-
 	return (
-		<button className={classes.button} onClick={onClick}>
-			<div className={classes.content}>
-				<div className={classes.icon}>{icon}</div>
-				<span>{text}</span>
-			</div>
-		</button>
+		<Link href={DISCORD_INVITE_LINK} className={classes.text}>
+			<button className={classes.button} type="button">
+				<div className={classes.content}>
+					<div className={classes.icon}>{icon}</div>
+					<span>{text}</span>
+				</div>
+			</button>
+		</Link>
 	)
 }
